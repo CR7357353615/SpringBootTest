82ed7c0dbe7471b25b760af27e3b1af4af8d5d16
CR7357353615 <357353615@qq.com>
2018-06-20 18:00:26 +0800

    edit1

--- src/main/java/com/lho/demo/controller/UserController.java
+++ src/main/java/com/lho/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.mybatis.spring.annotation.MapperScan;
@@ -13,14 +15,20 @@ import com.lho.demo.service.IUserService;
 @RestController
 @SpringBootApplication(scanBasePackages="com.lho")
 @MapperScan("com.lho.demo.dao")
-public class UserController {
+public class UserController extends SpringBootServletInitializer {
 	@Autowired
 	@Qualifier("userServiceImpl")
 	IUserService userService;
-	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserController.class);
+	}
+
 	public static void main(String[] args) {
-        SpringApplication.run(UserController.class, args);
-    }
+		SpringApplication.run(UserController.class, args);
+	}
+
 	@RequestMapping("/print")
 	public String printUser(){
 		return "123";
